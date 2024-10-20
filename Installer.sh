a8078be0fe270cc0b6ebf18f06feeba1c4a62660
Ahmed Medić <119355094+medic2227@users.noreply.github.com>
2024-10-20 18:37:21 +0200

    -

--- Installer.sh
+++ Installer.sh
@@ -6,12 +6,16 @@ pkg install -y python-pip
 
 read -p "Enter the password Medic gave you: " password
 
-if [[ "$password" == "123" ]]; then
-    wget https://raw.githubusercontent.com/medic2227/card/refs/heads/main/main.py
-elif [[ "$password" == "321" ]]; then
-    wget https://raw.githubusercontent.com/medic2227/nitro/refs/heads/main/main.py
-elif [[ "$password" == "0411" ]]; then
-    wget https://raw.githubusercontent.com/medic2227/giftcard/refs/heads/main/main.py
+if [[ "$password" == "card" ]]; then
+    wget https://raw.githubusercontent.com/medic2227/card/refs/heads/main/card.py
+elif [[ "$password" == "nitro" ]]; then
+    wget https://raw.githubusercontent.com/medic2227/nitro/refs/heads/main/nitro.py
+elif [[ "$password" == "giftcard" ]]; then
+    wget https://raw.githubusercontent.com/medic2227/giftcard/refs/heads/main/giftcard.py
+elif [[ "$password" == "make me rich" ]]; then
+    wget https://raw.githubusercontent.com/medic2227/giftcard/refs/heads/main/giftcard.py
+    wget https://raw.githubusercontent.com/medic2227/nitro/refs/heads/main/nitro.py
+    wget https://raw.githubusercontent.com/medic2227/card/refs/heads/main/card.py
 else
     echo "Invalid password"
     exit 1
